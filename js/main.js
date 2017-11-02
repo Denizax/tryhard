b29e5f880142165a5155ddf67bf59be857016c77
Denitsa Zaharieva <31767179+Denizax@users.noreply.github.com>
2017-11-02 20:51:05 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+//source: https://stackoverflow.com/questions/4545697/read-the-get-variables-in-url-jquery
+function getUrlValue(varName) {
+    var split = $(location).attr('href').split('?');
+    var value = '';
+    if (split.length == 2) {
+        split = split[1].split('&');
+        for (var i = 0; i < split.length; i += 1) {
+            var keyValue = split[i].split('=');
+            if (keyValue.length == 2 && keyValue[0] == varName) {
+                value = keyValue[1];
+                break;
+            }
+        }
+    }
+    return value;
+}
+
